2aa4a7af26716004e0824d0899a46507d99a8578
godfreycomputing <46703780+godfreycomputing@users.noreply.github.com>
2019-08-16 10:00:10 -0400

    Uploaded Full Solution

---
+++ DataAccess/ISQLiteDataAccess.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess
+{
+    namespace DataAccess.SQLiteDataAccess
+    {
+        using SQLite;
+        using System;
+        using System.Collections.Generic;
+
+        public interface ISQLiteDataAccess
+        {
+            void DeleteData<T>(object model) where T : class;
+            List<T> GetAllData<T>() where T : class, new();
+            SQLiteConnection GetConnection();
+            void SaveData<T>(object model) where T : class;
+            void UpdateData<T>(object model) where T : class;
+
+            string databasePath { get; set; }
+        }
+    }
+
+}
---
+++ DataAccess/SQLiteDataAccess.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace DataAccess.SQLiteDataAccess
+{
+    using SQLite;
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
+
+    public class SQLiteDataAccess : DataAccess.SQLiteDataAccess.ISQLiteDataAccess
+    {
+        public void DeleteData<T>(object model) where T : class
+        {
+            using (SQLiteConnection connection = this.GetConnection())
+            {
+                connection.CreateTable<T>(0);
+                connection.Delete(model);
+            }
+        }
+
+        public List<T> GetAllData<T>() where T : class, new()
+        {
+            using (SQLiteConnection connection = this.GetConnection())
+            {
+                connection.CreateTable<T>(0);
+                return connection.Table<T>().ToList();
+            }
+        }
+
+        public SQLiteConnection GetConnection() =>
+            new SQLiteConnection(this.databasePath, true, null);
+
+        public void SaveData<T>(object model) where T : class
+        {
+            using (SQLiteConnection connection = this.GetConnection())
+            {
+                connection.CreateTable<T>(0);
+                connection.Insert(model);
+            }
+        }
+
+        public void UpdateData<T>(object model) where T : class
+        {
+            using (SQLiteConnection connection = this.GetConnection())
+            {
+                connection.CreateTable<T>(0);
+                connection.Update(model);
+            }
+        }
+
+        public string databasePath { get; set; }
+    }
+}
